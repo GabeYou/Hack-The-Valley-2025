ccea369b96122ca415bdba36bdf365dbe27ebc80
Baehr3 <andy.huang12345@outlook.com>
2025-10-05 09:14:45 -0400

    Remove useless stuff and disable lint

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // Disable ESLint during production builds
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  // Disable TypeScript build errors during production builds
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   /* config options here */
 };
 
